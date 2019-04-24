12d7dfcf8331ac09193448cfb42b974a8394fbfb
Kennnyshiwa <kstjohn@kstjohn.us>
2019-04-24 16:31:29 -0400

    fix indents

--- imperialtoolkit/imperialtoolkit.py
+++ imperialtoolkit/imperialtoolkit.py
@@ -120,8 +120,8 @@ class ImperialToolkit(commands.Cog):
     @commands.command()
     async def _update(self):
         async with aiohttp.ClientSession(loop=self.bot.loop) as cs:
-        await cs.post(
-            f"https://botsfordiscord.com/api/bots/{self.id}",
-            json={"server count": self.guilds},
-            header={"Authorization": c404406caed32deccb251b6147ac83c0a981c0b1f24db77936b70119ab417a3b7ec65447144adb38c39c0cb9234e6965e5779591f5d2ef2f1eeb4d9d12e813cc, "content-type": "application/json"},
-        )
+            await cs.post(
+                f"https://botsfordiscord.com/api/bots/{self.id}",
+                json={"server count": self.guilds},
+                header={"Authorization": c404406caed32deccb251b6147ac83c0a981c0b1f24db77936b70119ab417a3b7ec65447144adb38c39c0cb9234e6965e5779591f5d2ef2f1eeb4d9d12e813cc, "content-type": "application/json"},
+            )

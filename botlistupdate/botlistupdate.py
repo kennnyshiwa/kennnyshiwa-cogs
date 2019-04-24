7908487e2f373c7df599a352f79526181d6b8e3f
Kennnyshiwa <kstjohn@kstjohn.us>
2019-04-24 16:44:02 -0400

    fix indents

--- botlistupdate/botlistupdate.py
+++ botlistupdate/botlistupdate.py
@@ -13,11 +13,11 @@ class BotListUpdate(commands.Cog):
 
 @checks.isowner()
 @commands.command()
-    async def _update(self):
-        async with aiohttp.ClientSession(loop=self.bot.loop) as cs:
-            await cs.post(
-                f"https://botsfordiscord.com/api/bots/{self.id}",
-                json={"server count": self.guilds},
-                header={"Authorization": c404406caed32deccb251b6147ac83c0a981c0b1f24db77936b70119ab417a3b7ec65447144adb38c39c0cb9234e6965e5779591f5d2ef2f1eeb4d9d12e813cc, "content-type": "application/json"},
-            )
-            await channel.send("Update sent to BotsForDiscord")
+async def _update(self):
+    async with aiohttp.ClientSession(loop=self.bot.loop) as cs:
+        await cs.post(
+            f"https://botsfordiscord.com/api/bots/{self.id}",
+            json={"server count": self.guilds},
+            header={"Authorization": c404406caed32deccb251b6147ac83c0a981c0b1f24db77936b70119ab417a3b7ec65447144adb38c39c0cb9234e6965e5779591f5d2ef2f1eeb4d9d12e813cc, "content-type": "application/json"},
+        )
+        await channel.send("Update sent to BotsForDiscord")

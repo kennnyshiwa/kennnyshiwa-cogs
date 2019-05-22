2e6fe4054043dbf522ece6e4c74905b8f46992f9
Kennnyshiwa <kstjohn@kstjohn.us>
2019-05-21 20:52:20 -0400

    trackerinfo update to include more tracker statues

--- trackerinfo/trackerinfo.py
+++ trackerinfo/trackerinfo.py
@@ -25,3 +25,93 @@ class Trackerinfo(commands.Cog):
         embed.set_footer(text="0 means down and 1 means up")
         
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def ggn(self, ctx):
+        """gets info on ptp"""
+        async with self.session.get("https://ggn.trackerstatus.info/api/status/") as r:
+            data = await r.json()
+        color = await ctx.embed_color()
+        website = data["Website"]
+        irc = data["IRC"]
+        ircannounce = data["IRCTorrentAnnouncer"]
+        embed = discord.Embed(description="Status of PTP tracker", color=color)
+        embed.set_image(url="https://ggn.trackerstatus.info/images/logo.png")
+        embed.add_field(name="Website", value="{}".format(website))
+        embed.add_field(name="IRC", value="{}".format(irc))
+        embed.add_field(name="IRC Torrent Announcer", value="{}".format(ircannounce))
+        embed.set_footer(text="0 means down and 1 means up")
+        
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def red(self, ctx):
+        """gets info on ptp"""
+        async with self.session.get("https://red.trackerstatus.info/api/status/") as r:
+            data = await r.json()
+        color = await ctx.embed_color()
+        website = data["Website"]
+        irc = data["IRC"]
+        ircannounce = data["IRCTorrentAnnouncer"]
+        embed = discord.Embed(description="Status of PTP tracker", color=color)
+        embed.set_image(url="https://red.trackerstatus.info/images/logo.png")
+        embed.add_field(name="Website", value="{}".format(website))
+        embed.add_field(name="IRC", value="{}".format(irc))
+        embed.add_field(name="IRC Torrent Announcer", value="{}".format(ircannounce))
+        embed.set_footer(text="0 means down and 1 means up")
+        
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def btn(self, ctx):
+        """gets info on ptp"""
+        async with self.session.get("https://btn.trackerstatus.info/api/status/") as r:
+            data = await r.json()
+        color = await ctx.embed_color()
+        website = data["Website"]
+        irc = data["IRC"]
+        ircannounce = data["Barney"]
+        embed = discord.Embed(description="Status of PTP tracker", color=color)
+        embed.set_image(url="https://btn.trackerstatus.info/images/logo.png")
+        embed.add_field(name="Website", value="{}".format(website))
+        embed.add_field(name="IRC", value="{}".format(irc))
+        embed.add_field(name="IRC Torrent Announcer", value="{}".format(ircannounce))
+        embed.set_footer(text="0 means down and 1 means up")
+        
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def btn(self, ctx):
+        """gets info on ptp"""
+        async with self.session.get("https://btn.trackerstatus.info/api/status/") as r:
+            data = await r.json()
+        color = await ctx.embed_color()
+        website = data["Website"]
+        irc = data["IRC"]
+        ircannounce = data["Barney"]
+        embed = discord.Embed(description="Status of PTP tracker", color=color)
+        embed.set_image(url="https://btn.trackerstatus.info/images/logo.png")
+        embed.add_field(name="Website", value="{}".format(website))
+        embed.add_field(name="IRC", value="{}".format(irc))
+        embed.add_field(name="IRC Torrent Announcer", value="{}".format(ircannounce))
+        embed.set_footer(text="0 means down and 1 means up")
+        
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def mtv(self, ctx):
+        """gets info on ptp"""
+        async with self.session.get("https://mtv.trackerstatus.info/api/status/") as r:
+            data = await r.json()
+        color = await ctx.embed_color()
+        website = data["Website"]
+        irc = data["IRC"]
+        ircannounce = data["IRCTorrentAnnouncer"]
+        embed = discord.Embed(description="Status of PTP tracker", color=color)
+        embed.set_image(url="https://mtv.trackerstatus.info/images/logo.png")
+        embed.add_field(name="Website", value="{}".format(website))
+        embed.add_field(name="IRC", value="{}".format(irc))
+        embed.add_field(name="IRC Torrent Announcer", value="{}".format(ircannounce))
+        embed.set_footer(text="0 means down and 1 means up")
+        
+        await ctx.send(embed=embed)

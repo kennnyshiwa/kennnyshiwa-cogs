126dab2c487b4e4d16ec496024ab5a30a251a76e
Kennnyshiwa <kstjohn@kstjohn.us>
2019-05-21 20:56:40 -0400

    accidentally included a duplicate command

--- trackerinfo/trackerinfo.py
+++ trackerinfo/trackerinfo.py
@@ -81,24 +81,6 @@ class Trackerinfo(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def btn(self, ctx):
-        """gets info on ptp"""
-        async with self.session.get("https://btn.trackerstatus.info/api/status/") as r:
-            data = await r.json()
-        color = await ctx.embed_color()
-        website = data["Website"]
-        irc = data["IRC"]
-        ircannounce = data["Barney"]
-        embed = discord.Embed(description="Status of PTP tracker", color=color)
-        embed.set_image(url="https://btn.trackerstatus.info/images/logo.png")
-        embed.add_field(name="Website", value="{}".format(website))
-        embed.add_field(name="IRC", value="{}".format(irc))
-        embed.add_field(name="IRC Torrent Announcer", value="{}".format(ircannounce))
-        embed.set_footer(text="0 means down and 1 means up")
-        
-        await ctx.send(embed=embed)
-
-    @commands.command()
     async def mtv(self, ctx):
         """gets info on ptp"""
         async with self.session.get("https://mtv.trackerstatus.info/api/status/") as r:

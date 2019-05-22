143395fa2bca7d3fcbd840e28eab46d8bca841ca
Kennnyshiwa <kstjohn@kstjohn.us>
2019-05-21 21:33:59 -0400

    fix f strings

--- trackerinfo/trackerinfo.py
+++ trackerinfo/trackerinfo.py
@@ -15,8 +15,11 @@ class Trackerinfo(commands.Cog):
             data = await r.json()
         color = await ctx.embed_color()
         website = data["Website"]
+        website = (f'Website is {"down" if website == "0" else "up"}')  
         irc = data["IRC"]
+        irc = (f'IRC is {"down" if irc == "0" else "up"}') 
         ircannounce = data["IRCTorrentAnnouncer"]
+        ircannounce = (f'IRC Announce is {"down" if ircannounce == "0" else "up"}') 
         embed = discord.Embed(description="Status of PTP tracker", color=color)
         embed.set_image(url="https://ptp.trackerstatus.info/images/logo.png")
         embed.add_field(name="Website", value="{}".format(website))
@@ -33,8 +36,11 @@ class Trackerinfo(commands.Cog):
             data = await r.json()
         color = await ctx.embed_color()
         website = data["Website"]
+        website = (f'Website is {"down" if website == "0" else "up"}')  
         irc = data["IRC"]
+        irc = (f'IRC is {"down" if irc == "0" else "up"}') 
         ircannounce = data["IRCTorrentAnnouncer"]
+        ircannounce = (f'IRC Announce is {"down" if ircannounce == "0" else "up"}') 
         embed = discord.Embed(description="Status of PTP tracker", color=color)
         embed.set_image(url="https://ggn.trackerstatus.info/images/logo.png")
         embed.add_field(name="Website", value="{}".format(website))
@@ -51,8 +57,11 @@ class Trackerinfo(commands.Cog):
             data = await r.json()
         color = await ctx.embed_color()
         website = data["Website"]
+        website = (f'Website is {"down" if website == "0" else "up"}') 
         irc = data["IRC"]
+        irc = (f'IRC is {"down" if irc == "0" else "up"}') 
         ircannounce = data["IRCTorrentAnnouncer"]
+        ircannounce = (f'IRC Announce is {"down" if ircannounce == "0" else "up"}') 
         embed = discord.Embed(description="Status of PTP tracker", color=color)
         embed.set_image(url="https://red.trackerstatus.info/images/logo.png")
         embed.add_field(name="Website", value="{}".format(website))
@@ -69,8 +78,11 @@ class Trackerinfo(commands.Cog):
             data = await r.json()
         color = await ctx.embed_color()
         website = data["Website"]
+        website = (f'Website is {"down" if website == "0" else "up"}') 
         irc = data["IRC"]
+        irc = (f'IRC is {"down" if irc == "0" else "up"}') 
         ircannounce = data["Barney"]
+        ircannounce = (f'IRC Announce is {"down" if ircannounce == "0" else "up"}') 
         embed = discord.Embed(description="Status of PTP tracker", color=color)
         embed.set_image(url="https://btn.trackerstatus.info/images/logo.png")
         embed.add_field(name="Website", value="{}".format(website))
@@ -87,8 +99,11 @@ class Trackerinfo(commands.Cog):
             data = await r.json()
         color = await ctx.embed_color()
         website = data["Website"]
+        website = (f'Website is {"down" if website == "0" else "up"}') 
         irc = data["IRC"]
+        irc = (f'IRC is {"down" if irc == "0" else "up"}') 
         ircannounce = data["IRCTorrentAnnouncer"]
+        ircannounce = (f'IRC Announce is {"down" if ircannounce == "0" else "up"}') 
         embed = discord.Embed(description="Status of PTP tracker", color=color)
         embed.set_image(url="https://mtv.trackerstatus.info/images/logo.png")
         embed.add_field(name="Website", value="{}".format(website))

394922b3fcca17ea9d1a6bb3b2f571c1c752c543
Kennnyshiwa <kstjohn@kstjohn.us>
2019-11-02 20:16:38 -0400

    [lastfm] config > json loads

--- lastfm/lastfm.py
+++ lastfm/lastfm.py
@@ -4,7 +4,7 @@ import urllib
 import discord
 import aiohttp
 import os
-from redbot.core import commands
+from redbot.core import commands, Config
 from redbot.core.i18n import Translator
 
 _ = Translator('Last_FM', __file__)
@@ -16,16 +16,17 @@ BaseCog = getattr(commands, "Cog", object)
 
 
 class LastFM(BaseCog):
+    
+    default_member_settings = {"username": ""}
+
+    default_user_settings = default_member_settings
+
     def __init__(self, bot):
         self.bot = bot
+        self.config = Config.get_conf(self, 376564057517457408, force_registration=True)
+        self.config.register_member(**self.default_member_settings)
+        self.config.register_user(**self.default_user_settings)
 
-        self.path = str(datam.cog_data_path(self)).replace('\\', '/')
-        self.settings_file = 'settings'
-
-        self.check_folder()
-        self.check_file()
-
-        self.bot.loop.create_task(self.load_settings())
 
         self.lf_gateway = 'http://ws.audioscrobbler.com/2.0/'
 
@@ -33,38 +34,6 @@ class LastFM(BaseCog):
         self.payload['api_key'] = 'c44979d5d86ff515ba9fba378c610474'
         self.payload['format'] = 'json'
 
-    def save_json(self, filename, data):
-        with open(self.path + '/{}.json'.format(filename), encoding='utf-8', mode="w") as f:
-            json.dump(data, f, indent=4, sort_keys=True, separators=(',', ' : '))
-        return data
-
-    def load_json(self, filename):
-        with open(self.path + '/{}.json'.format(filename), encoding='utf-8', mode="r") as f:
-            data = json.load(f)
-        return data
-
-    def check_folder(self):
-        if not os.path.exists(self.path):
-            os.mkdir(self.path)
-
-    def check_file(self):
-        if not os.path.exists(self.path + '/{}.json'.format(self.settings_file)):
-            self.save_json(self.settings_file, {})
-
-    def generate_default_settings(self):
-        if 'users' not in self.settings or 'servers' not in self.settings:
-            self.settings = {}
-            self.settings['users'] = {}
-            self.settings['servers'] = {}
-            self.save_json(self.settings_file, self.settings)
-        return True
-
-    async def load_settings(self):
-        self.settings = self.load_json(self.settings_file)
-        self.generate_default_settings()
-
-    async def save_settings(self):
-        self.save_json(self.settings_file, self.settings)
 
     async def _url_decode(self, url):
         # Fuck non-ascii URLs!!!@##$@
@@ -89,7 +58,7 @@ class LastFM(BaseCog):
             payload['autocorrect'] = autocorrect
 
         session = aiohttp.ClientSession(connector=aiohttp.TCPConnector())
-        async with session.get(self.lf_gateway, params=payload) as r:
+        async with session.get(self.lf_gateway, params=payload) as r:        
             data = await r.json()
         session.close()
         return data
@@ -104,98 +73,100 @@ class LastFM(BaseCog):
     #
     # }
 
-    @commands.command(pass_context=True, name='nowplaying')
-    async def _nowplaying(self, context):
+    @commands.command(name='nowplaying')
+    async def _nowplaying(self, ctx):
         """Shows the current played song"""
-        if str(context.author.id) in self.settings['users']:
-            username = self.settings['users'][str(context.author.id)]
+        username = await self.config.user(ctx.author).username()
 
-            method = 'user.getRecentTracks'
-            limit = 1
-            response = await self._api_request(method=method, username=username, limit=limit)
-            user = response['recenttracks']['@attr']['user']
-            track = response['recenttracks']['track'][0]
-            if '@attr' in track:
-                if track['@attr']['nowplaying'] == 'true':
-                    artist = track['artist']['#text']
-                    artist_url = await self._url_decode('https://www.last.fm/music/{}'.format(artist.replace(' ', '+')))
-                    song = track['name']
-                    track_url = await self._url_decode(track['url'])
+        method = 'user.getRecentTracks'
+        limit = 1
+        response = await self._api_request(method=method, username=username, limit=limit) 
+        user = response['recenttracks']['@attr']['user']
+        track = response['recenttracks']['track'][0]
+        if '@attr' in track:
+            if track['@attr']['nowplaying'] == 'true':
+                artist = track['artist']['#text']
+                artist_url = await self._url_decode('https://www.last.fm/music/{}'.format(artist.replace(' ', '+')))
+                song = track['name']
+                track_url = await self._url_decode(track['url'])
 
-                    album = track['album']['#text']
-                    album_url = await self._url_decode('https://www.last.fm/music/{}/{}'.format(artist.replace(' ', '+'), album.replace(' ', '+')))
+                album = track['album']['#text']
+                album_url = await self._url_decode('https://www.last.fm/music/{}/{}'.format(artist.replace(' ', '+'), album.replace(' ', '+')))
 
-                    image = track['image'][-1]['#text']
-                    tags = await self._api_request(method='track.getTopTags', track=song, artist=artist, autocorrect=1)
-                    trackinfo = await self._api_request(method='track.getInfo', track=song, artist=artist, username=username)
+                image = track['image'][-1]['#text']
+                tags = await self._api_request(method='track.getTopTags', track=song, artist=artist, autocorrect=1)
+                trackinfo = await self._api_request(method='track.getInfo', track=song, artist=artist, username=username)
 
-                    if 'userplaycount' in trackinfo['track']:
-                        playcount = trackinfo['track']['userplaycount']
-                    else:
-                        playcount = '0'
+                if 'userplaycount' in trackinfo['track']:
+                    playcount = trackinfo['track']['userplaycount']
+                else:
+                    playcount = '0'
 
-                    if 'error' not in tags:
-                        tags = ', '.join(['[{}]({})'.format(tag['name'], tag['url']) for tag in tags['toptags']['tag'][:10]])
-                    else:
-                        tags = None
+                if 'error' not in tags:
+                    tags = ', '.join(['[{}]({})'.format(tag['name'], tag['url']) for tag in tags['toptags']['tag'][:10]])
+                else:
+                    tags = None
 
-                    song = [song if len(song) < 18 else song[:18] + '...'][0]
-                    artist = [artist if len(artist) < 18 else artist[:18] + '...'][0]
-                    album = [album if len(album) < 18 else album[:18] + '...'][0]
+                song = [song if len(song) < 18 else song[:18] + '...'][0]
+                artist = [artist if len(artist) < 18 else artist[:18] + '...'][0]
+                album = [album if len(album) < 18 else album[:18] + '...'][0]
 
-                    em = discord.Embed()
-                    em.set_thumbnail(url=image)
-                    em.add_field(name=_('**Artist**'), value='[{}]({})'.format(artist, artist_url))
-                    em.add_field(name=_('**Track**'), value='[{}]({})'.format(song, track_url))
-                    em.add_field(name=_('**Playcount**'), value='{}'.format(playcount))
-                    em.add_field(name=_('**Album**'), value='[{}]({})'.format(album, album_url))
-                    if tags:
-                        em.add_field(name=_('**Tags**'), value=tags, inline=False)
-                    await context.send(embed=em)
-            else:
-                await context.send(_('{} is not playing any song right now').format(user))
+                em = discord.Embed()
+                em.set_thumbnail(url=image)
+                em.add_field(name=_('**Artist**'), value='[{}]({})'.format(artist, artist_url))
+                em.add_field(name=_('**Track**'), value='[{}]({})'.format(song, track_url))
+                em.add_field(name=_('**Playcount**'), value='{}'.format(playcount))
+                em.add_field(name=_('**Album**'), value='[{}]({})'.format(album, album_url))
+                if tags:
+                    em.add_field(name=_('**Tags**'), value=tags, inline=False)
+                await ctx.send(embed=em)
+        else:
+            await ctx.send(_('{} is not playing any song right now').format(user))
 
-    @commands.group(pass_context=True, name='lastfm', aliases=['lf'])
-    async def _lastfm(self, context):
+    @commands.group(name='lastfm', aliases=['lf'])
+    async def _lastfm(self, ctx):
         """Get Last.fm statistics of a user."""
 
-    @_lastfm.command(pass_context=True, name='set')
-    async def _set(self, context, username: str):
+    @_lastfm.command(name='set')
+    async def _set(self, ctx, username: str):
         """Set a username"""
         method = 'user.getInfo'
         response = await self._api_request(method=method, username=username)
         if 'error' not in response:
-            self.settings['users'][str(context.author.id)] = username
-            await self.save_settings()
+            await self.config.user(ctx.author).username.set(str(username))
             message = _('Username set')
         else:
             message = response['message']
-        await context.send(message)
+        await ctx.send(message)
+    
+    @_lastfm.command(name='check')
+    async def _check(self, ctx):
+        username = await self.config.user(ctx.author).username()
+        await ctx.send(username)
 
-    @_lastfm.command(pass_context=True, name='recent')
-    async def _recent(self, context):
+    @_lastfm.command(name='recent')
+    async def _recent(self, ctx):
         """Shows recent tracks"""
-        if str(context.author.id) in self.settings['users']:
-            username = self.settings['users'][str(context.author.id)]
-            limit = '10'
-            method = 'user.getRecentTracks'
-            response = await self._api_request(method=method, username=username, limit=limit)
-            if 'error' not in response:
-                user = response['recenttracks']['@attr']['user']
-                author = context.author
-                text = ''
-                for i, track in enumerate(response['recenttracks']['track'][:8], 1):
-                    artist = track['artist']['#text']
-                    song = track['name']
+        username = await self.config.user(ctx.author).username()
+        limit = '10'
+        method = 'user.getRecentTracks'
+        response = await self._api_request(method=method, username=username, limit=limit)
+        if 'error' not in response:
+            user = response['recenttracks']['@attr']['user']
+            author = ctx.author
+            text = ''
+            for i, track in enumerate(response['recenttracks']['track'][:8], 1):
+                artist = track['artist']['#text']
+                song = track['name']
 
-                    url = await self._url_decode(track['url'])
-                    artist = [artist if len(artist) < 25 else artist[:25] + '...'][0]
-                    song = [song if len(song) < 25 else song[:25] + '...'][0]
-                    text += _('`{}`{:<5}**[{}]({})** — **[{}]({})**\n').format(str(i), '', artist, 'https://www.last.fm/music/{}'.format(artist.replace(' ', '+')), song, url)
-                em = discord.Embed(description=text)
-                avatar = author.avatar_url if author.avatar else author.default_avatar_url
-                em.set_author(name=_('{}\'s Recent Tracks').format(user), icon_url=avatar, url='http://www.last.fm/user/{}/library'.format(user))
-                await context.send(embed=em)
-            else:
-                message = response['message']
-                await context.send(message)
+                url = await self._url_decode(track['url'])
+                artist = [artist if len(artist) < 25 else artist[:25] + '...'][0]
+                song = [song if len(song) < 25 else song[:25] + '...'][0]
+                text += _('`{}`{:<5}**[{}]({})** — **[{}]({})**\n').format(str(i), '', artist, 'https://www.last.fm/music/{}'.format(artist.replace(' ', '+')), song, url)
+            em = discord.Embed(description=text)
+            avatar = author.avatar_url if author.avatar else author.default_avatar_url
+            em.set_author(name=_('{}\'s Recent Tracks').format(user), icon_url=avatar, url='http://www.last.fm/user/{}/library'.format(user))
+            await ctx.send(embed=em)
+        else:
+            message = response['message']
+            await ctx.send(message)

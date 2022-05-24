294e1fa1ac447676289c24164ace8e578dc114c2
Cerealkilla699 <106120601+Cerealkilla699@users.noreply.github.com>
2022-05-24 07:13:37 -0400

    Update autogallery.py

--- autogallery/autogallery.py
+++ autogallery/autogallery.py
@@ -104,5 +104,5 @@ class Autogallery(Cog):
             embed.set_author(name=message.author, icon_url=str(message.author.avatar_url))
             embed.set_footer(text=message.channel)
             embed.set_image(url=attachment.url)
-            embed.add_field(name="link:", value=[here](message.jump_url), inline=True)
+            embed.add_field(name="link:", value=message.jump_url, inline=True)
             await gallerychannel.send(embed=embed)
